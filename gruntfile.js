1ebd8f272c91ce170c10a42a555a2de2c7dfda0e
RB Isaacson <nerdyrasa@users.noreply.github.com>
2017-02-22 10:15:57 -0600

    Integrate bower into the workflow.

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,78 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
 
-    concat : {
+    concat: {
       options: {
         separator: '\n\n//-----------------------------------------------------\n',
         banner: '\n\n//-----------------------------------------------------\n'
       },
-      dist : {
+      dist: {
         src: ['components/scripts/*.js'],
         dest: 'builds/development/js/script.js'
       }
-    }
+    }, // concat
+
+    bower_concat: {
+      all: {
+        dest: {
+          'js': 'builds/development/js/_bower.js',
+          'css': 'builds/development/css/_bower.css'
+        }
+      }
+    }, // bower_concat
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: [{
+          src: 'components/sass/style.scss',
+          dest: 'builds/development/css/style.css'
+        }]
+      }
+    }, // sass
+
+    wiredep: {
+      task: {
+        src: 'builds/development/**/*.html'
+      }
+    }, // wiredep
+
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 3000,
+          base: 'builds/development/',
+          livereload: true
+        }
+      }
+    }, // connect
+
+    watch: {
+      options: {
+        spawn: false,
+        livereload: true
+      },
+      scripts: {
+        files: ['builds/development/**/*.html',
+          'components/scripts/**/*.js',
+          'components/sass/**/*.scss'],
+        tasks: ['concat', 'sass']
+      }
+    }  // watch
+
   });  // initConfig
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-bower-concat');
+
+  grunt.registerTask('default', ['wiredep', 'bower_concat', 'concat', 'sass', 'connect','watch']);
 
 };  // wrapper function
 
